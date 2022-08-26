9fc974f2ef2919e286b91b16ebc221ce696ea114
Nataly <nataly.krotchenko@gmail.com>
2022-08-26 15:05:48 +0300

    add lab1

---
+++ task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> dict1 = new Dictionary<int, string>();
+            dict1.Add(1, "One");
+            dict1.Add(2, "Two");
+            dict1.Add(3, "Three");
+            Console.WriteLine("Перший словник ");
+            foreach (var person in dict1)
+            {
+                Console.WriteLine($"key: {person.Key}  value: {person.Value}");
+            }
+          
+            Console.WriteLine("-----------------------------------");
+            List<string> list = new List<string>() { "Tom", "Bob", "Sam" };
+            Console.WriteLine("Список");
+            foreach (string i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Dictionary<string, string> dict2 = list.ToDictionary(x => x, x => x);
+            dict2["Tom"] = dict1[1];
+            dict2["Bob"] = dict1[2];
+            dict2["Sam"] = dict1[3];
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Результат ");
+            foreach (var person in dict2)
+            {
+                Console.WriteLine($"key: {person.Key}  value: {person.Value}");
+            }
+            //Console.WriteLine(String.Join(Environment.NewLine, dict2));
+            Console.WriteLine();
+            string jsonString = JsonSerializer.Serialize(dict2);
+
+            Console.WriteLine(jsonString);
+
+        }
+    }
+
+}
---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var output = input.Select((x, i) => ((i + 1) * x));
+            var output1 = output.Where(x => x >= 10 && x <= 99);
+            var output2 = output1.Reverse();
+            Console.WriteLine("До виконання");
+
+            foreach (int i in input)
+                Console.WriteLine(i);
+
+            Console.WriteLine("Добуток"); 
+
+            foreach (int i in output)
+                Console.WriteLine(i);
+
+            Console.WriteLine("Видалення не двозначних елементів");
+            foreach (int i in output1)
+                Console.WriteLine(i);
+
+            Console.WriteLine("Зворотній порядок");
+            foreach (int i in output2)
+                Console.WriteLine(i);
+
+        }
+    }
+    
+    }
+    
